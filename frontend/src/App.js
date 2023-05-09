1059c692a482b0cb1d65832e23b72f6698067f75
AbhinavYdv <abhinavyadavdev@gmail.com>
2023-05-09 22:22:12 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className='container'>
+        <Routes>
+          <Route/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
